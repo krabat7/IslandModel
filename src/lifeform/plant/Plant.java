9a5d08402073358d1bc798582f5b338aa0120a51
Simon <simon.cheremisin@gmail.com>
2023-06-29 10:21:27 +0300

    В класс Plant добавлено поле максимального количества растения на 1 локации.

--- src/lifeform/plant/Plant.java
+++ src/lifeform/plant/Plant.java
@@ -1,15 +1,12 @@
 package lifeform.plant;
 
 public class Plant {
-    double foodAmount = 1; // Вес растения в кг
-    public double getFoodAmount() {
-        return foodAmount;
+    private double weight = 1; // Вес растения в кг
+    private int maxPopulation = 200; // Максимальное количество вида животного на 1 клетке
+    public double getWeight() {
+        return weight;
     }
-
-    public Plant(double foodAmount) {
-        this.foodAmount = foodAmount;
-    }
-    public void decreaseFoodAmount(double amount) {
-        foodAmount -= amount;
+    public int getMaxPopulation() {
+        return maxPopulation;
     }
 }
