f36a9e51225a921338e3aa18b56387bd5e30995c
Simon <simon.cheremisin@gmail.com>
2023-06-29 10:21:54 +0300

    Абстрактный класс стат публичным.

--- src/lifeform/animal/predator/Predator.java
+++ src/lifeform/animal/predator/Predator.java
@@ -2,7 +2,7 @@ package lifeform.animal.predator;
 
 import lifeform.animal.Animal;
 
-abstract class Predator extends Animal {
+public abstract class Predator extends Animal {
     public Predator(double weight, int step, double maxFood, int maxPopulation) {
         super(weight, step, maxFood, maxPopulation);
     }
