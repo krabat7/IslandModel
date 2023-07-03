de2aa27f85ea318b6019bc791deeeb638c56685f
Simon <semion.cheremisin@gmail.com>
2023-07-03 07:06:49 +0300

    Класс теперь наследуется от LifeForm, в который помещены 3 параметра - weight, maxPopulation, name. Добавлен параметр hp - здоровье животного в настоящий момент. Добавлены параметры столбца и строки. Добавлены соответствующие геттеры и сеттеры.

--- src/lifeform/animal/Animal.java
+++ src/lifeform/animal/Animal.java
@@ -1,33 +1,50 @@
 package lifeform.animal;
 
 import field.Location;
+import lifeform.LifeForm;
 
-public abstract class Animal {
-    double weight; // Вес одного животного, кг
-    int step; // Скорость перемещения, не более чем, клеток за ход
-    double maxFood; // Максимальное количество килограммов пищи нужно животному для полного насыщения
-    int maxPopulation; // Максимальное количество вида животного на 1 клетке
-    public Animal(double weight, int step, double maxFood, int maxPopulation){
-        this.weight = weight;
+public abstract class Animal extends LifeForm {
+    final private int step; // Скорость перемещения, не более чем, клеток за ход
+    final private double maxHp; // Максимальное количество килограммов пищи нужно животному для полного насыщения
+    private double hp; // Количество здоровья животного
+    private int row;
+    private int column;
+    public Animal(double weight, int step, double maxHp, int maxPopulation, String name){
+        super(weight, maxPopulation, name);
         this.step = step;
-        this.maxFood = maxFood;
-        this.maxPopulation = maxPopulation;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-    public int getStep() {
-        return step;
-    }
-    public double getMaxFood() {
-        return maxFood;
-    }
-    public int getMaxPopulation() {
-        return maxPopulation;
+        this.maxHp = maxHp;
+        this.hp = maxHp; // На старте максимальное количество здоровья
     }
 
     public abstract void eat(Object food);
     public abstract void multiply(Animal partner);
     public abstract void move(Location[][] locations);
+
+    public int getStep() {
+        return step;
+    }
+
+    public double getHp() {
+        return hp;
+    }
+    public void setHp(double hp) {
+        this.hp = hp;
+    }
+    public double getMaxHp() {
+        return maxHp;
+    }
+
+    public int getRow() {
+        return row;
+    }
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+    public void setColumn(int column) {
+        this.column = column;
+    }
 }
