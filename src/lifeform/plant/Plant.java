5501a62deaf20a0707c72f29df3a515f743f5899
Simon <semion.cheremisin@gmail.com>
2023-07-03 07:10:33 +0300

    Все параметры вынесены в класс предок, от которого теперь наследуется класс Plant.

--- src/lifeform/plant/Plant.java
+++ src/lifeform/plant/Plant.java
@@ -1,12 +1,9 @@
 package lifeform.plant;
 
-public class Plant {
-    private double weight = 1; // Вес растения в кг
-    private int maxPopulation = 200; // Максимальное количество вида животного на 1 клетке
-    public double getWeight() {
-        return weight;
-    }
-    public int getMaxPopulation() {
-        return maxPopulation;
+import lifeform.LifeForm;
+
+public class Plant extends LifeForm {
+    public Plant(){
+        super(1, 200, "Plant");
     }
 }
