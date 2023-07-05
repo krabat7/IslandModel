eec88c47f4a7c581cbf30647980081b610f15ea0
Simon <simon.cheremisin@gmail.com>
2023-07-05 18:29:47 +0300

    Реализован переопределенный метод eat().

--- src/lifeform/animal/herbivore/Herbivore.java
+++ src/lifeform/animal/herbivore/Herbivore.java
@@ -1,9 +1,51 @@
 package lifeform.animal.herbivore;
 
+import error.ObjectNotLifeFormException;
+import field.IslandField;
+import field.Location;
+import lifeform.LifeForm;
 import lifeform.animal.Animal;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class Herbivore extends Animal {
     public Herbivore(double weight, int step, double maxHp, int maxPopulation, String name) {
         super(weight, step, maxHp, maxPopulation, name);
+    }
+
+    @Override
+    public void eat(Object food) {
+        double chanceToEat;
+        LifeForm lifeForm = null;
+
+        if (food instanceof LifeForm) {
+            lifeForm = (LifeForm) food;
+        } else{
+            try {
+                throw new ObjectNotLifeFormException("Объект не является животными/растением.");
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        String foodName = lifeForm.getName();
+
+        switch (foodName) {
+            case "Plant" -> chanceToEat = 1;
+            default -> chanceToEat = 0;
+        }
+
+        boolean animalEatFood = ThreadLocalRandom.current().nextDouble() < chanceToEat;
+
+        if (animalEatFood){
+            setHp(Math.min((getHp() + lifeForm.getWeight()), getMaxHp())); // Показатель здоровья повышается после съедения
+
+            Location location = IslandField.getInstance().getLocation(lifeForm.getRow(), lifeForm.getColumn()); // Животное/растение удаляется из списка обиталей локации после съедения
+            if (lifeForm instanceof Animal){
+                Animal animal = (Animal) lifeForm;
+                location.removeAnimal(animal);
+            }else{
+                location.removePlant();
+            }
+        }
     }
 }
