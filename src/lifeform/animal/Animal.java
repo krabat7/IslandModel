d213e14254f2c145b698420e8af2992169bc7500
Simon <simon.cheremisin@gmail.com>
2023-07-05 18:30:28 +0300

    Реализован метод move().

--- src/lifeform/animal/Animal.java
+++ src/lifeform/animal/Animal.java
@@ -1,14 +1,14 @@
 package lifeform.animal;
 
-import field.Location;
+import field.IslandField;
 import lifeform.LifeForm;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class Animal extends LifeForm {
     final private int step; // Скорость перемещения, не более чем, клеток за ход
     final private double maxHp; // Максимальное количество килограммов пищи нужно животному для полного насыщения
     private double hp; // Количество здоровья животного
-    private int row;
-    private int column;
     public Animal(double weight, int step, double maxHp, int maxPopulation, String name){
         super(weight, maxPopulation, name);
         this.step = step;
@@ -18,7 +18,51 @@ public abstract class Animal extends LifeForm {
 
     public abstract void eat(Object food);
     public abstract void multiply(Animal partner);
-    public abstract void move(Location[][] locations);
+    public void move() {
+        int randomCells = ThreadLocalRandom.current().nextInt(1, getStep() + 1);
+
+        // Генерируем случайное направление (вверх, вниз, влево или вправо)
+        int direction = ThreadLocalRandom.current().nextInt(4);
+
+        // Вычисляем новые координаты в зависимости от направления
+        int newRow = getRow();
+        int newColumn = getColumn();
+
+        switch (direction) {
+            case 0 -> // Вверх
+                    newRow -= randomCells;
+            case 1 -> // Вниз
+                    newRow += randomCells;
+            case 2 -> // Влево
+                    newColumn -= randomCells;
+            case 3 -> // Вправо
+                    newColumn += randomCells;
+        }
+
+        // Проверяем, чтобы новые координаты не выходили за границы поля
+        while (newRow < 0 || IslandField.getInstance().getNumRows() >= 20 || newColumn < 0 || newColumn >= IslandField.getInstance().getNumColumns()) {
+            randomCells = ThreadLocalRandom.current().nextInt(1, getStep() + 1);
+            direction = ThreadLocalRandom.current().nextInt(4);
+
+            newRow = getRow();
+            newColumn = getColumn();
+
+            switch (direction) {
+                case 0 -> // Вверх
+                        newRow -= randomCells;
+                case 1 -> // Вниз
+                        newRow += randomCells;
+                case 2 -> // Влево
+                        newColumn -= randomCells;
+                case 3 -> // Вправо
+                        newColumn += randomCells;
+            }
+        }
+
+        // Обновляем новые координаты
+        setRow(newRow);
+        setColumn(newColumn);
+    }
 
     public int getStep() {
         return step;
@@ -32,19 +76,5 @@ public abstract class Animal extends LifeForm {
     }
     public double getMaxHp() {
         return maxHp;
-    }
-
-    public int getRow() {
-        return row;
-    }
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-    public void setColumn(int column) {
-        this.column = column;
     }
 }
