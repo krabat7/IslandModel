8950da23427727e42eca3bbaaaa9abee67550843
Simon <simon.cheremisin@gmail.com>
2023-07-05 18:33:48 +0300

    В класс добалены поля row и column для кажого растения/животного и их геттеры и сеттеры.

--- src/lifeform/LifeForm.java
+++ src/lifeform/LifeForm.java
@@ -4,6 +4,8 @@ public class LifeForm {
     public double weight; // Вес животного/растения в кг
     public int maxPopulation; // Максимальное количество вида животного/растения на 1 клетке
     public String name; // Имя животного/растения
+    private int row;
+    private int column;
     public LifeForm(double weight, int maxPopulation, String name){
         this.weight = weight;
         this.maxPopulation = maxPopulation;
@@ -17,5 +19,18 @@ public class LifeForm {
     }
     public String getName() {
         return name;
+    }
+    public int getRow() {
+        return row;
+    }
+    public int getColumn() {
+        return column;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+    public void setColumn(int column) {
+        this.column = column;
     }
 }
