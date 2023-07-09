79e6166ba0e16bd04cd1a14936f2e72cb29cb6ab
Simon <simon.cheremisin@gmail.com>
2023-07-09 14:42:05 +0300

    Добавлена логика старта программы.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,16 @@
 package main;
 
+import simulation.IslandSimulation;
+
 public class Main {
     public static void main(String[] args) {
-
+        printStartWords();
+        IslandSimulation.getInstance();
+    }
+    private static void printStartWords(){
+        System.out.println("--------------------------");
+        System.out.println("Загрузка симуляции острова...");
+        System.out.println("--------------------------");
+        System.out.println();
     }
 }
