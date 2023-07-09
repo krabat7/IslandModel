a3424f531504f463da41050ab42666590c904959
Simon <simon.cheremisin@gmail.com>
2023-07-09 21:19:40 +0300

    Добавил фильтр в списке животных, чтобы убрать тех кто не передвигается.

--- src/simulation/thread/animalLifecycleTask/task/AnimalMoveTask.java
+++ src/simulation/thread/animalLifecycleTask/task/AnimalMoveTask.java
@@ -8,11 +8,13 @@ import java.util.List;
 public class AnimalMoveTask implements Runnable{
     @Override
     public void run() {
-        List<Animal> animals = IslandField.getInstance().getAllAnimals();
+        List<Animal> animals = IslandField.getInstance().getAllAnimals().stream().filter(c -> c.getStep() > 0).toList();
         System.out.println("MOVE");
         for (Animal animal : animals){
+            System.out.println(animal + " РРРРРРРРРР " + animal.getRow() + " РРРРРРРР " + animal.getColumn());
             animal.move();
+            System.out.println(animal + " HAHAHAHAH " + animal.getRow() + " HAHAHAH " + animal.getColumn());
         }
-        System.out.println("MOVE2");
+        System.out.println("MOVE_END");
     }
 }
