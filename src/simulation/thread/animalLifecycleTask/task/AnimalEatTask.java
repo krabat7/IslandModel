3ed594370dce89758bc47779f295877664259413
Simon <simon.cheremisin@gmail.com>
2023-07-09 21:23:08 +0300

    Добавлено условие при котором, животное которое съедает пищу не может иметь 0 потребности в пищи(maxHp).

--- src/simulation/thread/animalLifecycleTask/task/AnimalEatTask.java
+++ src/simulation/thread/animalLifecycleTask/task/AnimalEatTask.java
@@ -33,26 +33,33 @@ public class AnimalEatTask implements Runnable {
         if (countAnimalsStart > 0) {   //проверка на умерли ли все животные
             for (int i = 0; i < animals.size(); i++) {
                 Animal currentAnimal = animals.get(i);
-                System.out.println("START_EAT31");
-                Location location = IslandField.getInstance().getLocation(currentAnimal.getRow(), currentAnimal.getColumn());
-                System.out.println("EAT32");
-                Random random = ThreadLocalRandom.current();
-                System.out.println("EAT33");
-                List<LifeForm> locationLifeForms = location.getLifeForms();
-                System.out.println("EAT34");
-                LifeForm lifeFormToEat = locationLifeForms.get(random.nextInt(locationLifeForms.size()));
-                System.out.println("EAT35");
-                System.out.println(currentAnimal + " __ " + lifeFormToEat);
-                System.out.println();
-                boolean isEaten = currentAnimal.eat(lifeFormToEat);
-                System.out.println("EAT355");
-                if (isEaten) {
-                    animals.remove(lifeFormToEat);
-                    animalsAreEaten.incrementAndGet(); // Кол-во съеденных животных
+                if (currentAnimal.getMaxHp() > 0) {
+                    System.out.println("START_EAT31");
+                    System.out.println(currentAnimal);
+                    System.out.println(currentAnimal.getRow());
+                    System.out.println(currentAnimal.getColumn());
+                    Location location = IslandField.getInstance().getLocation(currentAnimal.getRow(), currentAnimal.getColumn());
+                    System.out.println("EAT32");
+                    Random random = ThreadLocalRandom.current();
+                    System.out.println("EAT33");
+                    List<LifeForm> locationLifeForms = location.getLifeForms();
+                    System.out.println("EAT34");
+                    if (locationLifeForms.size() > 0) {
+                        LifeForm lifeFormToEat = locationLifeForms.get(random.nextInt(locationLifeForms.size()));
+                        System.out.println("EAT35");
+                        System.out.println(currentAnimal + " __ " + lifeFormToEat);
+                        System.out.println();
+                        boolean isEaten = currentAnimal.eat(lifeFormToEat);
+                        System.out.println("EAT355");
+                        if (isEaten) {
+                            animals.remove(lifeFormToEat);
+                            animalsAreEaten.incrementAndGet(); // Кол-во съеденных животных
+                        }
+                        System.out.println("EAT36");
+                        animals.remove(currentAnimal);
+                        System.out.println("EAT37");
+                    }
                 }
-                System.out.println("EAT36");
-                animals.remove(currentAnimal);
-                System.out.println("EAT37");
             }
         } else {
             System.out.println("EATGGGGGG");
@@ -62,7 +69,7 @@ public class AnimalEatTask implements Runnable {
         System.out.println("EAT4");
         countAnimals.set(countAnimalsStart - animalsAreEaten.get()); // Кол-во животных на острове
         latch.countDown();
-        System.out.println("END_EAT2");
+        System.out.println("END_EAT");
     }
 
     public AtomicInteger getAnimalsAreEaten() {
